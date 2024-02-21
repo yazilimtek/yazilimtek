6335accb68566611960e26919764dda2b036677c
Yazılım Teknisyeni <74830789+yazilimtek@users.noreply.github.com>
2024-02-21 22:04:10 +0300

    Update blog.js

--- js/blog.js
+++ js/blog.js
@@ -14,7 +14,7 @@ document.addEventListener("DOMContentLoaded", function() {
                 file = file.trim();
                 if (file.endsWith(".html") && file !== "index.html") {
                     // Blog yazısının dosya yolunu oluşturun
-                    var postFilePath = blogFolder + file;
+                    var postFilePath = "yazilimteknisyeni.com.tr/" + blogFolder + file;
 
                     // Yeni bir div oluşturun ve içeriğini blog yazısıyla doldurun
                     var postDiv = document.createElement("div");
