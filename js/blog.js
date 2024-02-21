7378fc5d9b98eb9d81ae4256cd2bb509c30f7cfb
Yazılım Teknisyeni <74830789+yazilimtek@users.noreply.github.com>
2024-02-21 22:57:27 +0300

    Update blog.js

--- js/blog.js
+++ js/blog.js
@@ -1,12 +1,12 @@
-// Klasörün URL'si
-var folderUrl = "https://yazilimteknisyeni.com.tr//blog/";
+// Blog klasörü URL'si
+var blogFolderUrl = "https://yazilimteknisyeni.com.tr/blog/";
 
-// Fetch API kullanarak içerik alma
-fetch(folderUrl)
+// Fetch API kullanarak blog klasöründeki içerikleri alma
+fetch(blogFolderUrl)
   .then(response => {
     // HTTP durum kodunu kontrol et
     if (!response.ok) {
-      throw new Error('Klasör yüklenirken bir hata oluştu: ' + response.status);
+      throw new Error('Blog klasörü yüklenirken bir hata oluştu: ' + response.status);
     }
     // Metni al ve döndür
     return response.text();
@@ -24,24 +24,22 @@ fetch(folderUrl)
 
     // Rastgele 4 dosya seç
     var randomFiles = [];
-    while (randomFiles.length < 4) {
+    while (randomFiles.length < 4 && htmlFiles.length > 0) {
       var randomIndex = Math.floor(Math.random() * htmlFiles.length);
-      var randomFile = htmlFiles[randomIndex];
-      if (!randomFiles.includes(randomFile)) {
-        randomFiles.push(randomFile);
-      }
+      var randomFile = htmlFiles.splice(randomIndex, 1)[0];
+      randomFiles.push(randomFile);
     }
 
     // Seçilen dosyaların linklerini sayfaya ekleyin
-    var blogLinksContainer = document.getElementById("blog-links");
-    randomFiles.forEach((url, index) => {
+    var blogPostsContainer = document.getElementById("blog-posts");
+    randomFiles.forEach(url => {
       var linkElement = document.createElement('a');
       linkElement.href = url;
-      linkElement.textContent = "Yazı " + (index + 1);
-      blogLinksContainer.appendChild(linkElement);
-      blogLinksContainer.appendChild(document.createElement('br'));
+      linkElement.textContent = url; // Link metni olarak URL'i kullandık, isteğinize göre değiştirebilirsiniz
+      blogPostsContainer.appendChild(linkElement);
+      blogPostsContainer.appendChild(document.createElement('br'));
     });
   })
   .catch(error => {
-    console.error('Klasör yüklenirken bir hata oluştu:', error);
+    console.error('Blog klasörü yüklenirken bir hata oluştu:', error);
   });
