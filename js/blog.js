b5b291b3c7880d11c90e898453946153a4ddb929
Yazılım Teknisyeni <74830789+yazilimtek@users.noreply.github.com>
2024-02-21 22:33:15 +0300

    Update blog.js

--- js/blog.js
+++ js/blog.js
@@ -1,26 +1,34 @@
-document.addEventListener("DOMContentLoaded", async function() {
+document.addEventListener("DOMContentLoaded", function () {
     var blogPostsContainer = document.getElementById("blog-posts");
     var blogFolder = "https://yazilimteknisyeni.com.tr/blog/";
-    var filesResponse = await fetch(blogFolder);
-    var files = await filesResponse.text();
 
-    files.split("\n").forEach(async function(file) {
-        file = file.trim();
-        if (file.endsWith(".html")) {
-            var postFilePath = blogFolder + file;
-            var postResponse = await fetch(postFilePath);
-            var postHTML = await postResponse.text();
-            var tempDiv = document.createElement("div");
-            tempDiv.innerHTML = postHTML;
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", blogFolder, true);
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            var files = xhr.responseText.split("\n");
 
-            var postLink = postFilePath;
-            var postTitle = tempDiv.querySelector("title").innerText;
+            // Tüm dosyaların içeriğini tek bir istekte alın
+            Promise.all(files.map(file => fetch(blogFolder + file).then(response => response.text())))
+                .then(contents => {
+                    contents.forEach(content => {
+                        var tempDiv = document.createElement("div");
+                        tempDiv.innerHTML = content;
 
-            var postDiv = document.createElement("div");
-            postDiv.classList.add("blog-post");
-            postDiv.innerHTML = "<h2><a href='" + postLink + "'>" + postTitle + "</a></h2>";
+                        var postLink = blogFolder + content;
+                        var postTitle = tempDiv.querySelector("title").innerText;
 
-            blogPostsContainer.appendChild(postDiv);
+                        var postDiv = document.createElement("div");
+                        postDiv.classList.add("blog-post");
+                        postDiv.innerHTML = "<h2><a href='" + postLink + "'>" + postTitle + "</a></h2>";
+
+                        blogPostsContainer.appendChild(postDiv);
+                    });
+                })
+                .catch(error => {
+                    console.error("Hata oluştu: ", error);
+                });
         }
-    });
+    };
+    xhr.send();
 });
