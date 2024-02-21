c18edd150cdf4a0fa1ab3e72d6be218373bb0cc1
Yazılım Teknisyeni <74830789+yazilimtek@users.noreply.github.com>
2024-02-21 21:02:29 +0300

    Update blog.js

--- js/blog.js
+++ js/blog.js
@@ -1,41 +1,54 @@
 document.addEventListener("DOMContentLoaded", function () {
     var blogPostsContainer = document.getElementById("blog-posts");
 
-    // Blog yazısının dosya yolu
-    var postFilePath = "blog/nvidia-rtx-chatbot.html";
+    // Blog yazılarının bulunduğu klasörün yolu
+    var blogFolder = "blog/";
 
-    // Blog yazısını içeriği çekmek için bir AJAX isteği gönderin
+    // AJAX isteği göndermek için yeni bir XMLHttpRequest nesnesi oluştur
     var xhr = new XMLHttpRequest();
-    xhr.open("GET", postFilePath, true);
+    xhr.open("GET", blogFolder, true);
     xhr.onreadystatechange = function () {
         if (xhr.readyState === 4 && xhr.status === 200) {
-            // AJAX isteği tamamlandığında, blog yazısının içeriğini işleyin
-            var postHTML = xhr.responseText;
+            // AJAX isteği tamamlandığında, klasör içeriğini işle
+            var files = xhr.responseText.split("\n");
 
-            // Blog yazısının içeriğini analiz etmek için bir geçici div oluşturun
-            var tempDiv = document.createElement("div");
-            tempDiv.innerHTML = postHTML;
+            // Son 4 yazıyı göstermek için bir döngü oluşturun
+            for (var i = Math.max(0, files.length - 4); i < files.length; i++) {
+                var file = files[i].trim();
+                if (file.endsWith(".html")) {
+                    // Blog yazısının dosya yolu
+                    var postFilePath = blogFolder + file;
 
-            // Blog yazısının dosya adını alın
-            var postFileName = postFilePath;
+                    // Blog yazısını içeriği çekmek için bir AJAX isteği gönderin
+                    var xhrPost = new XMLHttpRequest();
+                    xhrPost.open("GET", postFilePath, true);
+                    xhrPost.onreadystatechange = function () {
+                        if (xhrPost.readyState === 4 && xhrPost.status === 200) {
+                            // AJAX isteği tamamlandığında, blog yazısının içeriğini işleyin
+                            var postHTML = xhrPost.responseText;
 
-            // Blog yazısının linkini, başlığını, resmini ve meta description'ını alın
-            var postLink = postFileName;
-            var postTitle = tempDiv.querySelector("meta[name='og:title']").getAttribute("content");
-            var postImage = tempDiv.querySelector("meta[property='og:image']").getAttribute("content");
-            var postMetaDescription = tempDiv.querySelector("meta[name='description']").getAttribute("content");
+                            // Blog yazısının içeriğini analiz etmek için bir geçici div oluşturun
+                            var tempDiv = document.createElement("div");
+                            tempDiv.innerHTML = postHTML;
 
-            // Blog yazısını göstermek için bir div oluşturun
-            var postDiv = document.createElement("div");
-            postDiv.innerHTML = "<a href='" + postLink + "'><img src='" + postImage + "' alt='" + postTitle + "'></a>" +
-                "<h2><a href='" + postLink + "'>" + postTitle + "</a></h2>" +
-                "<p>" + postMetaDescription + "</p>";
+                            // Blog yazısının linkini, başlığını, resmini ve meta description'ını alın
+                            var postTitle = tempDiv.querySelector("meta[name='og:title']").content;
+                            var postImage = tempDiv.querySelector("meta[property='og:image']").content;
+                            var postLink = "siteniz.com/" + file;
 
-            // Blog yazısını göstermek için ana div'e ekleyin
-            blogPostsContainer.appendChild(postDiv);
-            console.log(postHTML);
+                            // Blog yazısını göstermek için bir div oluşturun
+                            var postDiv = document.createElement("div");
+                            postDiv.innerHTML = "<a href='" + postLink + "'><img src='" + postImage + "' alt='" + postTitle + "'></a>" +
+                                                "<h2><a href='" + postLink + "'>" + postTitle + "</a></h2>";
+
+                            // Blog yazısını göstermek için ana div'e ekleyin
+                            blogPostsContainer.appendChild(postDiv);
+                        }
+                    };
+                    xhrPost.send();
+                }
+            }
         }
     };
     xhr.send();
-    
 });
