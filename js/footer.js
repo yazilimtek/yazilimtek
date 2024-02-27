21cb86e639868cf125105bb20e12c399d9616cbc
Yazılım Teknisyeni <74830789+yazilimtek@users.noreply.github.com>
2024-02-27 17:43:11 +0300

    Create footer.js

---
+++ js/footer.js
@@ -0,0 +1,14 @@
+ <!-- Vendor JS Files -->
+  <script src="https://yazilimteknisyeni.com.tr/assets/vendor/purecounter/purecounter_vanilla.js"></script>
+  <script src="https://yazilimteknisyeni.com.tr/assets/vendor/aos/aos.js"></script>
+  <script src="https://yazilimteknisyeni.com.tr/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="https://yazilimteknisyeni.com.tr/assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="https://yazilimteknisyeni.com.tr/assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="https://yazilimteknisyeni.com.tr/assets/vendor/swiper/swiper-bundle.min.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="https://yazilimteknisyeni.com.tr/assets/js/main.js"></script>
+
+<!-- Start W3Counter Web Analytics -->
+  <script src="https://www.w3counter.com/tracker.js?id=151587"></script>
+<!-- End W3Counter Web Analytics -->
