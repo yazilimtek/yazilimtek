b718901a0b0d8fcf566e0ead9cc8f377e55f9a41
Yazılım Teknisyeni <74830789+yazilimtek@users.noreply.github.com>
2024-02-27 22:46:26 +0300

    Create footer.js

---
+++ js/footer.js
@@ -0,0 +1,27 @@
+var script1 = document.createElement('script');
+script1.src = 'https://yazilimteknisyeni.com.tr/assets/js/main.js';
+document.body.appendChild(script1);
+
+var script2 = document.createElement('script');
+script2.src = 'https://yazilimteknisyeni.com.tr/assets/vendor/aos/aos.js';
+document.body.appendChild(script2);
+
+var script3 = document.createElement('script');
+script3.src = 'https://yazilimteknisyeni.com.tr/assets/vendor/swiper/swiper-bundle.min.js';
+document.body.appendChild(script3);
+
+var script4 = document.createElement('script');
+script4.src = 'https://yazilimteknisyeni.com.tr/assets/vendor/glightbox/js/glightbox.min.js';
+document.body.appendChild(script4);
+
+var script5 = document.createElement('script');
+script5.src = 'https://yazilimteknisyeni.com.tr/assets/vendor/purecounter/purecounter_vanilla.js';
+document.body.appendChild(script5);
+
+var script6 = document.createElement('script');
+script6.src = 'https://yazilimteknisyeni.com.tr/assets/vendor/isotope-layout/isotope.pkgd.min.js';
+document.body.appendChild(script6);
+
+var script7 = document.createElement('script');
+script7.src = 'https://yazilimteknisyeni.com.tr/assets/vendor/bootstrap/js/bootstrap.bundle.min.js';
+document.body.appendChild(script7);
