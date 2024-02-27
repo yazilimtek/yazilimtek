b16d49d9dbe9bab095e56796b0b2ea45bbde8fb3
Yazılım Teknisyeni <74830789+yazilimtek@users.noreply.github.com>
2024-02-27 20:51:01 +0300

    Update footer.js

--- js/footer.js
+++ js/footer.js
@@ -1,7 +1,13 @@
-import 'https://yazilimteknisyeni.com.tr/assets/vendor/purecounter/purecounter_vanilla.js';
-import 'https://yazilimteknisyeni.com.tr/assets/vendor/aos/aos.js';
-import 'https://yazilimteknisyeni.com.tr/assets/vendor/bootstrap/js/bootstrap.bundle.min.js';
-import 'https://yazilimteknisyeni.com.tr/assets/vendor/glightbox/js/glightbox.min.js';
-import 'https://yazilimteknisyeni.com.tr/assets/vendor/isotope-layout/isotope.pkgd.min.js';
-import 'https://yazilimteknisyeni.com.tr/assets/vendor/swiper/swiper-bundle.min.js';
-import 'https://yazilimteknisyeni.com.tr/assets/js/main.js';
+Promise.all([
+    loadScript('https://yazilimteknisyeni.com.tr/assets/vendor/aos/aos.js'),
+    loadScript('https://yazilimteknisyeni.com.tr/assets/vendor/swiper/swiper-bundle.min.js'),
+    loadScript('https://yazilimteknisyeni.com.tr/assets/vendor/glightbox/js/glightbox.min.js'),
+    loadScript('https://yazilimteknisyeni.com.tr/assets/vendor/purecounter/purecounter_vanilla.js'),
+    loadScript('https://yazilimteknisyeni.com.tr/assets/vendor/isotope-layout/isotope.pkgd.min.js'),
+    loadScript('https://yazilimteknisyeni.com.tr/assets/vendor/bootstrap/js/bootstrap.bundle.min.js')
+]).then(() => {
+    console.log('Tüm JavaScript dosyaları başarıyla yüklendi.');
+}).catch(error => {
+    console.error('Hata oluştu:', error);
+});
+
