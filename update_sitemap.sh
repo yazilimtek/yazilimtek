a649fb40bc396d3b15e09a6d68d43512fd98ffa6
Yazılım Teknisyeni <74830789+yazilimtek@users.noreply.github.com>
2024-03-01 21:43:37 +0300

    Update update_sitemap.sh

--- update_sitemap.sh
+++ update_sitemap.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Sitemap dosyasının yolu
-SITEMAP_FILE="sitemaps.xml"
+SITEMAP_FILE="sitemap.xml"
 
 echo "<url>" >> "$SITEMAP_FILE"
 echo "  <loc>https://yazilimteknisyeni.com.tr/</loc>" >> "$SITEMAP_FILE"
